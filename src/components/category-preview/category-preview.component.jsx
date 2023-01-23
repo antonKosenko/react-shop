6cf59298797888e13743ca2f1e5596bf79804494
AntonKosenko <anton.kosenko.mk@gmail.com>
2023-01-23 12:04:03 +0200

    finished 1.2

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,5 +1,4 @@
 import ProductCard from "../product-card/product-card.component";
-import { Link } from "react-router-dom";
 import { CategoryPreviewCotnainer, Title, Preview } from "./category-preview.styles";
 
 const CategoryPreview = ({ title, products }) => {
